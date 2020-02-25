2c551703887f11760d0dc6db1b873b582a8637ec
eshete <eshete@DESKTOP-HCFFR5C>
2020-02-25 13:58:14 -0500

    code change

--- More/src/test/java/learning/handelingCookie.java
+++ More/src/test/java/learning/handelingCookie.java
@@ -6,34 +6,32 @@ import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class handelingCookie {
-	WebDriver driver;
-	
-	public  void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver",utility.PathList.chromeDriver);
-		 driver = new ChromeDriver();
-     
-	// Go to the correct domain
-	driver.get("http://objectivewellness");
-	// Now set the cookie. This one's valid for the entire domain
-	Cookie cookie = new Cookie("key", "value");
-	driver.manage().addCookie(cookie);
 
-	// And now output all the available cookies for the current URL
-	Set<Cookie> allCookies = driver.manage().getCookies();
-	for (Cookie loadedCookie : allCookies) {
-	    System.out.println(String.format("%s -> %s", loadedCookie.getName(), loadedCookie.getValue()));
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", utility.PathList.chromeDriver);
+		WebDriver driver = new ChromeDriver();
+
+		// Go to the correct domain
+		driver.get("https://www.objectivewellness.com");
+		// Now set the cookie. This one's valid for the entire domain
+		Cookie cookie = new Cookie("key", "value");
+		driver.manage().addCookie(cookie);
+
+		// And now output all the available cookies for the current URL
+		Set<Cookie> allCookies = driver.manage().getCookies();
+		for (Cookie loadedCookie : allCookies) {
+			System.out.println(String.format(loadedCookie.getName(), loadedCookie.getValue()));
+		}
+
+		// You can delete cookies in 3 ways
+		// By name
+		// driver.manage().deleteCookieNamed("CookieName");
+		// By Cookie
+		// driver.manage().deleteCookie(loadedCookie);
+		// Or all of them
+		// driver.manage().deleteAllCookies();
+
 	}
-
-	// You can delete cookies in 3 ways
-	// By name
-	//driver.manage().deleteCookieNamed("CookieName");
-	// By Cookie
-	//driver.manage().deleteCookie(loadedCookie);
-	// Or all of them
-	//driver.manage().deleteAllCookies();
-
-}
 }
